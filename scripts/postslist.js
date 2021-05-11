82007bda5d6391623b73aa8aa5f1473918ad99ad
Archita Jain <jainarchita12@gmail.com>
2021-05-10 23:49:21 -0700

    Fixed delete post

--- scripts/postslist.js
+++ scripts/postslist.js
@@ -4,9 +4,11 @@ const deletePost2 = document.querySelector('#delPost2');
 const deletePost3 = document.querySelector('#delPost3');
 const deletePost4 = document.querySelector('#delPost4');
 const deletePost5 = document.querySelector('#delPost5');
+const deleteYes = document.querySelector('#yesDelete');
 
 const closeDelPost = document.querySelector('#dontDelete');
 const postContainer = document.querySelector('.posts-container');
+deleteYes.addEventListener('click', deletingPost);
 
 deletePost1.addEventListener('click', delPost);
 deletePost2.addEventListener('click', delPost);
@@ -17,8 +19,16 @@ deletePost5.addEventListener('click', delPost);
 closeDelPost.addEventListener('click', dontDeletePost);
 postContainer.addEventListener('click', savePostDetails);
 
-function delPost() {
+function delPost(e) {
     deletePostModal.style.display = 'block';
+    const id = parseInt(e.target.dataset.id);
+    sessionStorage.setItem('postId',id);
+}
+
+function deletingPost() {
+    const id = parseInt(sessionStorage.getItem('postId')) - 1;
+    document.querySelectorAll('.card-content')[id].remove();
+    deletePostModal.style.display = 'none';  
 }
 
 function dontDeletePost() {
